7f7f6aaf854ba6c8716fd5ade6822cf8a7bd0040
kin <1037671280@qq.com>
2020-10-13 20:29:54 +0800

    添加 gitee 部署

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ git add . &&
 git commit -m "update" &&
 git branch -M master &&
 git remote add origin git@github.com:Kinsj/kin-ui-vite.git &&
+git remote add gitee https://gitee.com/kinsj/kin-ui-vue3.git &&
 git push -f -u origin master &&
+git push -f -u gitee master &&
 cd -
-echo 'https://kinsj.github.io/kin-ui-vite/#/'
+echo 'https://kinsj.github.io/kin-ui-vite'
+echo 'http://kinsj.gitee.io/kin-ui-vue3'
